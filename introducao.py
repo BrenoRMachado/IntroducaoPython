43205ac0b6cf717bf7f54bf8780ffc6707596373
Breno Machado <burracha.6464@gmail.com>
2024-11-26 20:41:14 -0300

    adiciona  começo

---
+++ introducao.py
@@ -0,0 +1,13 @@
+
+variavel = "pedra"
+altura = 1.70
+nome = "Breno"
+# assim que comenta
+'''
+comentário alternativo?
+
+'''
+print(nome)
+
+print("meu nome é" + nome) # idetenção sem espaço
+print("meu nome é", nome)  # vírgula serve de espação
